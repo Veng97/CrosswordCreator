1cd3531880ff12e45e2cf7d3e6f415a974823656
Veng <xmortenvengx@gmail.com>
2024-12-07 20:19:09 +0100

    Enhance Flask GUI with dark mode styling and improved logging display

--- serve_with_gui.py
+++ serve_with_gui.py
@@ -2,9 +2,15 @@ import logging
 import webbrowser
 import threading
 import tkinter as tk
+from tkinter import ttk
 from tkinter import scrolledtext
 
 from serve import app, serve_flask_app
+
+
+PRIMARY_COLOR = "#333333"
+FONT_NORMAL = ("Helvetica", 14)
+FONT_BOLD = ("Helvetica", 14, "bold")
 
 
 class FlaskGUI(tk.Tk):
@@ -13,20 +19,34 @@ class FlaskGUI(tk.Tk):
         self.title("Crossword Helper (Backend)")
         self.geometry("600x400")
 
-        self.createLoggingWidget()
+        self.configureUI()
+        self.configureLogger()
 
         self.serve()
 
-    def createLoggingWidget(self):
-        # Create a Text widget to display logs
-        self.logger = scrolledtext.ScrolledText(self, state="disabled", wrap="word", height=20)
+    def configureUI(self):
+        # Set dark background and light text color for the window
+        self.configure(bg=PRIMARY_COLOR)
+
+        # Create a ttk style object for dark mode
+        style = ttk.Style(self)
+        style.configure('TFrame', background=PRIMARY_COLOR)
+        style.configure('TLabel', background=PRIMARY_COLOR, foreground='white', font=FONT_NORMAL)
+        style.configure('TButton', background='#4C4C4C', foreground='white', font=FONT_NORMAL)
+        style.configure('TScrolledText', background=PRIMARY_COLOR, foreground='white', font=FONT_NORMAL)
+
+        # Create a ScrolledText widget inside the frame with the custom dark style
+        self.logger = scrolledtext.ScrolledText(self, state="disabled", wrap="word", height=20, bg=PRIMARY_COLOR, fg="white", font=FONT_NORMAL)
         self.logger.pack(fill="both", padx=10, pady=10, expand=True)
+
+        # Configure tags for different log levels with color
         self.logger.tag_configure("DEBUG", foreground="cyan")
         self.logger.tag_configure("INFO", foreground="green")
         self.logger.tag_configure("WARNING", foreground="yellow")
         self.logger.tag_configure("ERROR", foreground="red")
-        self.logger.tag_configure("CRITICAL", foreground="red", font=("TkDefaultFont", 10, "bold"))
+        self.logger.tag_configure("CRITICAL", foreground="red", font=FONT_BOLD)
 
+    def configureLogger(self):
         # Redirect logs to the Text widget
         handler = logging.StreamHandler()
         handler.setFormatter(logging.Formatter(
